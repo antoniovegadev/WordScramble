8ad14e57cf7dbddfa436a085208839b18e5029f3
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-19 16:57:24 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Antonio Vega on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
